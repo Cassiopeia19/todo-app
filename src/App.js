86a9e3cf1ff5caaedb2e3404d88006bdad13670e
Jennie <cassiopeia5571@gmail.com>
2019-12-03 10:16:03 -0600

    updates thru delete todo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import './App.css';
-import TodoApp from './components/todo/TodoApp'
+import React, { Component } from "react";
+import TodoApp from "./components/todo/TodoApp";
+import "./App.css";
 import "./bootstrap.css";
 
 class App extends Component {
